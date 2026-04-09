ed32961c167d6c7342e7c8d93eceb9b8ea2bcadc
Prathamesh Wadile <3unnycr@gmail.com>
2026-04-09 16:20:32 +0530

    Auto wiring update

--- backend/core/module/module.go
+++ backend/core/module/module.go
@@ -4,6 +4,10 @@ import "github.com/gin-gonic/gin"
 
 type Module interface {
 	Name() string
+
+	// Initialize dependencies (DB, services, config, etc.)
 	Init(ctx *ModuleContext) error
+
+	// Register HTTP routes
 	RegisterRoutes(r *gin.RouterGroup)
 }
