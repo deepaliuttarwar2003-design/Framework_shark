85ed93cb0886d08d01d9f349246ff5abb25ce365
Prathamesh Wadile <3unnycr@gmail.com>
2026-04-20 13:41:54 +0530

    updated layout for suppressHydrationWarning

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,13 +23,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} min-h-full flex flex-col antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
